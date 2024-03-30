57e9ee69bffc50f782c0c226af1bd49d3fb284f5
전승혜 <jeonsh1001@naver.com>
2024-03-30 18:25:36 +0900

    feat: 그룹에서 인원 추가 완료

--- FrontEnd/lib/components/groups/GroupEmailFindField.dart
+++ FrontEnd/lib/components/groups/GroupEmailFindField.dart
@@ -5,18 +5,26 @@ import 'package:front/repository/api/ApiGroup.dart';
 
 class GroupEmailFindField extends StatefulWidget {
   final TextEditingController controller;
+  final groupId;
+  final members;
+  final Function(List<String>) onInvite;
 
   const GroupEmailFindField({
     Key? key,
     required this.controller,
+    required this.groupId,
+    required this.members,
+    required this.onInvite,
   }) : super(key: key);
 
   @override
   _GroupEmailFindFieldState createState() => _GroupEmailFindFieldState();
 }
+
 class _GroupEmailFindFieldState extends State<GroupEmailFindField> {
   final _formKey = GlobalKey<FormState>();
   Member? _searchResult;
+  List<String> inviteMember = [];
 
   @override
   Widget build(BuildContext context) {
@@ -33,10 +41,41 @@ class _GroupEmailFindFieldState extends State<GroupEmailFindField> {
                 onPressed: () async {
                   if (_formKey.currentState!.validate()) {
                     try {
-                      final response = await getMemberByEmail(widget.controller.text);
+                      final response =
+                          await getMemberByEmail(widget.controller.text);
                       if (response != null) {
                         setState(() {
                           _searchResult = Member.fromJson(response.data);
+                          if (_searchResult != null &&
+                              _searchResult!.kakaoId != null) {
+                            // 그룹 내에 이미 참가된 멤버인지 확인
+                            bool isAlreadyMember = widget.members.any(
+                                (member) =>
+                                    member.kakaoId == _searchResult!.kakaoId);
+                            // inviteMember 리스트 내에 이미 존재하는지 확인
+                            bool isAlreadyInvited =
+                                inviteMember.contains(_searchResult!.kakaoId);
+
+                            // 그룹 내에 있는 멤버일 경우 제외
+                            if (isAlreadyMember) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text("이미 참가한 멤버입니다",
+                                        style: TextStyle(color: Colors.red))),
+                              );
+                              widget.controller.clear();
+                            } else if (isAlreadyInvited) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text("이미 추가한 멤버입니다",
+                                        style: TextStyle(color: Colors.red))),
+                              );
+                              widget.controller.clear();
+                            } else {
+                              inviteMember.add(_searchResult!.kakaoId!);
+                              widget.controller.clear();
+                            }
+                          }
                         });
                       } else {
                         setState(() {
@@ -47,25 +86,39 @@ class _GroupEmailFindFieldState extends State<GroupEmailFindField> {
                       print("멤버 검색 중 오류 발생: $e");
                       setState(() {
                         _searchResult = null;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text("검색된 사람이 없습니다",
+                                  style: TextStyle(color: Colors.red))),
+                        );
                       });
                     }
                   }
                 },
-
               ),
             ),
             validator: (value) {
-              if (value == null || value.isEmpty || !EmailValidator.validate(value)) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !EmailValidator.validate(value)) {
                 return '유효한 이메일을 입력해주세요.';
               }
               return null;
             },
           ),
         ),
-        if (_searchResult != null) ...[
-          SizedBox(height: 20),
-          Text(_searchResult!.name, style: TextStyle(fontSize: 16)),
-        ],
+
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: TextButton(
+            child: Text('초대하기'),
+            onPressed: () {
+              widget
+                  .onInvite(inviteMember);
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
       ],
     );
   }
--- FrontEnd/lib/repository/api/ApiFcm.dart
+++ FrontEnd/lib/repository/api/ApiFcm.dart
@@ -2,3 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:front/repository/commons.dart';
 
 final api = ApiClient();
+
+Future sendNotiGroup(data) async {
+  final res = await api.post('/notification/group', data: data);
+  print(res.runtimeType);
+  return res.toString();
+}
