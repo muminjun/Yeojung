b0b321af1da1c696707ff9c06990baaf425c44af
전승혜 <jeonsh1001@naver.com>
2024-04-03 14:31:33 +0900

    fix: css수정완료

--- FrontEnd/lib/components/mains/MainNowTravel.dart
+++ FrontEnd/lib/components/mains/MainNowTravel.dart
@@ -57,6 +57,7 @@ class _NowTravelListState extends State<NowTravelList> {
       body: Center(
         child: Container(
           height: 200.h,
+          // width: ,
           child: CarouselSlider.builder(
             itemCount: ongoingGroups.length,
             itemBuilder: (context, index, realIndex) {
@@ -70,7 +71,7 @@ class _NowTravelListState extends State<NowTravelList> {
             },
             options: CarouselOptions(
                 autoPlay: false,
-                aspectRatio: 3.0,
+                aspectRatio: 2.5,
                 enlargeCenterPage: true,
                 viewportFraction: 0.6,
                 onPageChanged: (index, reason) {
